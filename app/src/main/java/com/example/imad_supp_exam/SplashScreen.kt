0e38025761a926538ccb2b4cd1ea0866806fdff3
aqeelahhh <ST10516749@myemeris.edu.za>
2026-07-28 10:39:28 +0200

    added a 3 second delay to splash screen

--- app/src/main/java/com/example/imad_supp_exam/SplashScreen.kt
+++ app/src/main/java/com/example/imad_supp_exam/SplashScreen.kt
@@ -1,12 +1,14 @@
 package com.example.imad_supp_exam
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreen(intent1: Intent) : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,9 +18,15 @@ class SplashScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Splash Screen
-        // Transition to Main Activity Screen in 3000ms from Splash screen
-        // 
+
+        // Transition to Main Activity Screen in 3000 milliseconds (3 seconds) from Splash screen
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            SplashScreen(intent)
+            finish() // Close current activity so user cannot go back to it
+        }, 3000)
+
+
 
 
     }
