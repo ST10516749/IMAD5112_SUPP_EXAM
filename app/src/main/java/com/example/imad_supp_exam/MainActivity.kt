c61d88745e4903b668aa98758224d59f4dba112a
aqeelahhh <ST10516749@myemeris.edu.za>
2026-07-28 11:33:03 +0200

    added buttons and backgrounds

--- app/src/main/java/com/example/imad_supp_exam/MainActivity.kt
+++ app/src/main/java/com/example/imad_supp_exam/MainActivity.kt
@@ -16,7 +16,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //
+        
+
+        // empty array
+        val items = arrayOf<String>("","","","","","","","")
+
+
+
 
     }
 
